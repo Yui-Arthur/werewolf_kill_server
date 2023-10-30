4c8eaa159ecf4117d8b4ed089004fd9f4bd880d4
Yui <75032673+Yui-Arthur@users.noreply.github.com>
2023-10-30 20:08:42 +0800

    API Agent id to name (#5)
    * fix get all info will not return died player info
    
    * turn API request and agent info agent id to name

--- werewolf_kill_server/models/agent.js
+++ werewolf_kill_server/models/agent.js
@@ -34,19 +34,19 @@ module.exports = {
                             color: color,promptDir: prompt_dir} , function(err, response){
             
             if(err){
-                console.log(`[${new Date(Date.now())}] - create agent failed , ${err.message}`) 
+                console.log(`[${new Date(Date.now())}] - create agent ${agent_name} failed , ${err.message}`) 
                 return route_back({status: false,  log:"create agent failed"})
             }
             else{
-                console.log(`[${new Date(Date.now())}] - create agent success : ${response["agentID"]}`) 
-                global.room_list[room_name]['agent'].push(response["agentID"])
+                console.log(`[${new Date(Date.now())}] - create agent ${agent_name} success ID : ${response["agentID"]}`) 
+                global.room_list[room_name]['agent'][agent_name] = response["agentID"]
                 return route_back({status: true,  log:"ok" , agentID : response["agentID"] })
                 
             }                    
         });
     },
 
-    delete_agent : async function(room_name , user_name, token , agent_id , route_back){
+    delete_agent : async function(room_name , user_name, token , agent_name , route_back){
         const client = new agent(config.agent_server_ip, grpc.credentials.createInsecure());
         token = token.replace('Bearer ', '')
         if(!await jwt_op.verify_room_jwt(token , room_name , true , user_name))
@@ -55,16 +55,15 @@ module.exports = {
         if(! global.grpc_server_check['status']['agent'])
             return route_back({status: false,  log:"agent server is not available"})
 
-        client.delete_agent({agentID : agent_id} , function(err, response){
+        client.delete_agent({agentID : global.room_list[room_name]['agent'][agent_name]} , function(err, response){
 
             if(err){
-                console.log(`[${new Date(Date.now())}] - delete agent ${agent_id} failed , ${err.message}`) 
+                console.log(`[${new Date(Date.now())}] - delete agent ${agent_name} failed , ${err.message}`) 
                 return route_back({status: false,  log:"delete agent failed"})
             }
             else{
-                console.log(`[${new Date(Date.now())}] - delete agent ${agent_id} success`) 
-                var index = global.room_list[room_name]['agent'].indexOf(agent_id);
-                global.room_list[room_name]['agent'].splice(index, 1)
+                console.log(`[${new Date(Date.now())}] - delete agent ${agent_name} success`) 
+                delete global.room_list[room_name]['agent'][agent_name]
                 return route_back({status: true,  log:"ok"})
             }                    
         });
@@ -75,16 +74,16 @@ module.exports = {
         if(! global.grpc_server_check['status']['agent'])
             return 
 
-        for(const agent_id of global.game_list[room_name]['agent']){
+        for(const [agent_name,agent_id] of Object.entries(global.game_list[room_name]['agent'])){
 
             client.get_agent_info({agentID : agent_id} , function(err, response){
     
                 if(err){
-                    console.log(`[${new Date(Date.now())}] - get agent ${agent_id} info failed , ${err.message}`) 
+                    console.log(`[${new Date(Date.now())}] - get agent ${agent_name} info failed , ${err.message}`) 
                 }
                 else{
-                    console.log(`[${new Date(Date.now())}] - get agent ${agent_id} info success`) 
-                    global.game_list[room_name]['agent_info'][agent_id] = response['agentInfo']
+                    console.log(`[${new Date(Date.now())}] - get agent ${agent_name} info success`) 
+                    global.game_list[room_name]['agent_info'][agent_name] = response['agentInfo']
                 }                    
             });
         }
--- werewolf_kill_server/routes/agent.js
+++ werewolf_kill_server/routes/agent.js
@@ -7,7 +7,8 @@ router.route('/api/agent/:room_name/:user_name')
         try{
             result =  await agent.create_agent(req.params.room_name, req.params.user_name , req.header('Authorization') , req.body , function (result){
                 if(result.status)
-                res.status(200).json(result)
+                // res.status(200).json(result)
+                res.sendStatus(200)
                 else
                     res.status(500).json({
                         Error : result.log
@@ -20,10 +21,10 @@ router.route('/api/agent/:room_name/:user_name')
         }
     })
 
-router.route('/api/agent/:room_name/:user_name/:agent_id')
+router.route('/api/agent/:room_name/:user_name/:agent_name')
     .delete(async function (req, res){
         try{
-            result =  await agent.delete_agent(req.params.room_name, req.params.user_name , req.header('Authorization') , req.params.agent_id , function (result){
+            result =  await agent.delete_agent(req.params.room_name, req.params.user_name , req.header('Authorization') , req.params.agent_name , function (result){
                 if(result.status)
                 res.sendStatus(200)
                 else
